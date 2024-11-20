27e3f7666d66cc22452f766d1ed39ec849f51166
STRATEG1C <strateg4k@gmail.com>
2024-11-20 19:53:33 +0100

    Add coin gethering

---
+++ php/gameMap.php
@@ -0,0 +1,221 @@
+<?php
+
+final class GameMap
+{
+    private const PLAYER_INDEX = 0;
+    private const PLAYER_DIRECTION_INDEX = 1;
+    private const ENEMY_INDEX = 0;
+    private const ENEMY_DIRECTION_INDEX = 1;
+
+    private const X = 1;
+    private const Y = 0;
+
+    private $map = [];
+    private $asteroidsPositions = [];
+    private $playerPosition = [0, 0];
+    private $playerDirection = '-';
+    private $coinsPositions = [];
+    private $enemyPositions = [];
+
+    public function __construct(array $map)
+    {
+        $this->map = $map;
+
+        foreach ($map as $rowIndex => $row) {
+            foreach ($row as $columnIndex => $cell) {
+                if ($cell === 'A') {
+                    $this->asteroidsPositions[] = [$rowIndex, $columnIndex];
+                } else if ($cell[self::PLAYER_INDEX] === 'P') {
+                    $this->playerPosition = [$rowIndex, $columnIndex];
+                    $this->playerDirection = $cell[self::PLAYER_DIRECTION_INDEX];
+                } else if ($cell[self::ENEMY_INDEX] === 'E') {
+                    $enemy = [$rowIndex, $columnIndex];
+                    $enemyPosition[self::ENEMY_DIRECTION_INDEX] = $cell[self::ENEMY_DIRECTION_INDEX];
+                    $this->enemyPositions[] = [$enemy, $enemyPosition];
+                } else if ($cell === 'C') {
+                    $this->coinsPositions[] = [$rowIndex, $columnIndex];
+                } else if ($cell === '_') {
+                    continue;
+                }
+            }
+        }
+    }
+
+    private function findShortestPath(array $start, array $end): ?array
+    {
+        $rows = count($this->map);
+        $cols = count($this->map[0]);
+        $directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+        $queue = [[$start]];
+        $visited = [$start];
+
+        while (!empty($queue)) {
+            $path = array_shift($queue);
+
+            $current = end($path);
+
+            if ($current === $end) {
+                return $path;
+            }
+
+            foreach ($directions as $direction) {
+                $newRow = $current[0] + $direction[0];
+                $newCol = $current[1] + $direction[1];
+
+                $next = [$newRow, $newCol];
+
+                if ($newRow >= 0 && $newRow < $rows &&
+                    $newCol >= 0 && $newCol < $cols &&
+                    !in_array($next, $visited) &&
+                    $this->map[$newRow][$newCol] !== 'A') {
+
+                    $visited[] = $next;
+                    $queue[] = array_merge($path, [$next]);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public function getPlayerPosition(): array
+    {
+        return $this->playerPosition;
+    }
+
+    public function getPlayerDirection(): string
+    {
+        return $this->playerDirection;
+    }
+
+    public function findPathsToCoins(): array
+    {
+        $paths = [];
+        foreach ($this->coinsPositions as $coinPosition) {
+            $path = $this->findShortestPath($this->playerPosition, $coinPosition);
+            if ($path !== null) {
+                $paths[] = [
+                    'path' => $path,
+                    'length' => count($path) - 1,
+                ];
+            }
+        }
+        return $paths;
+    }
+
+    public function findPathsToEnemies(): array
+    {
+        $paths = [];
+        foreach ($this->enemyPositions as $enemy) {
+            $path = $this->findShortestPath($this->playerPosition, $enemy[0]);
+            if ($path !== null) {
+                $paths[] = [
+                    'path' => $path,
+                    'length' => count($path) - 1,
+                ];
+            }
+        }
+        return $paths;
+    }
+
+    public function nextMoveToNearestCoin(): ?string
+    {
+        $coins = $this->findPathsToCoins();
+        if (empty($coins)) {
+            return null;
+        }
+
+        $currentLength = 99;
+        $coinPosition = null;
+        foreach ($coins as $coin) {
+            if ($coin['length'] < $currentLength) {
+                $currentLength = $coin['length'];
+                var_dump(json_encode($coin['path']));
+                $coinPosition = $coin['path'][1];
+            }
+        }
+
+        return $this->getStep($coinPosition);
+    }
+
+    private function movePlayer(string $direction): ?string {
+        $playerDirection = $this->getPlayerDirection();
+
+//        if ($playerDirection === 'N' && $direction === 'E') {
+//            return 'R';
+//        } else if ($playerDirection === 'N' && $direction === 'W' ) {
+//            return 'L';
+//        } else if ($playerDirection === 'N' && $direction === 'N') {
+//            return 'M';
+//        } else if ($playerDirection === 'N' && $direction === 'S') {
+//            return 'R';
+//        } else if ($playerDirection === 'S' && $direction === 'E') {
+//            return 'R';
+//        } else if ($playerDirection === 'S' && $direction === 'W') {
+//            return 'L';
+//        } else if ($playerDirection === 'S' && $direction === 'N') {
+//            return 'R';
+//        } else if ($playerDirection === 'S' && $direction === 'S') {
+//            return 'M';
+//        } else if ($playerDirection === 'E' && $direction === 'E') {
+//            return 'M';
+//        } else if ($playerDirection === 'E' && $direction === 'W') {
+//            return 'R';
+//        } else if ($playerDirection === 'E' && $direction === 'N') {
+//            return 'L';
+//        } else if ($playerDirection === 'E' && $direction === 'S') {
+//            return 'R';
+//        } else if ($playerDirection === 'W' && $direction === 'E') {
+//            return 'R';
+//        } else if ($playerDirection === 'W' && $direction === 'W') {
+//            return 'M';
+//        } else if ($playerDirection === 'W' && $direction === 'N') {
+//            return 'R';
+//        } else if ($playerDirection === 'W' && $direction === 'S') {
+//            return 'L';
+//        }
+
+        $crossingArray = [
+            'SW' => 'R',
+            'SE' => 'L',
+            'SN' => 'L',
+
+            'NW' => 'L',
+            'NE' => 'R',
+            'NS' => 'L',
+
+            'WN' => 'R',
+            'WS' => 'L',
+            'WE' => 'L',
+
+            'EN' => 'L',
+            'ES' => 'R',
+            'EW' => 'L'
+        ];
+
+        var_dump($playerDirection . $direction);
+
+        return $crossingArray[$playerDirection . $direction] ?? 'M';
+    }
+
+    private function getStep(array $nextPoint): ?string {
+        $position = $this->getPlayerPosition();
+
+//        var_dump($nextPoint);
+//        var_dump($position);
+//
+//        return null;
+
+        if ($nextPoint[self::X] > $position[self::X]) {
+            return $this->movePlayer('E');
+        } else if ($nextPoint[self::X] < $position[self::X]) {
+            return $this->movePlayer('W');
+        } else if ($nextPoint[self::Y] > $position[self::Y]) {
+            return $this->movePlayer('S');
+        } else {
+            return $this->movePlayer('N');
+        }
+    }
+
+}
--- php/index.php
+++ php/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'gameMap.php';
+
 $routes = [];
 
 function get(string $path, callable $callback)
@@ -34,8 +36,80 @@ get('/healthz', function () {
     return ['status' => 'OK'];
 });
 
-get('/', function () {
-    return ['data' => 'Hello, world !'];
+post('/move', function () {
+    // 1. Определить все сущности на карте
+    //  1.1 Определить положение своего корабля и направление (тут P - это наш корабль и вторая буква - направление)
+    //  1.2 Определить астероиды (сделать массив с астероидами и их координатами, [ [0,0], [0,1], [0,2] ... ]
+    //  1.3 Определить монеты (так же как и с астероидами)
+    //  1.4 Определить врагов (тут помимо координат, нужно направление, [ [0,0, 'W'], [0,1, 'S'], [0,2, 'N'] ... ])
+
+    $jsonData = json_decode(file_get_contents('php://input'), true);
+    $field = $jsonData['field'];
+    $narrowingIn = $jsonData['narrowingIn'];
+    $gameId = $jsonData['gameId'];
+
+    $gameMap = new GameMap($field);
+
+    $playerPosition = $gameMap->getPlayerPosition();
+    $playerDirection = $gameMap->getPlayerDirection();
+
+    $move = $gameMap->nextMoveToNearestCoin();;
+
+//    $closestCoinPath = null;
+//
+//    foreach ($pathsToCoins as $path) {
+//        if ($closestCoinPath === null || count($path) < count($closestCoinPath)) {
+//            $closestCoinPath = $path;
+//        }
+//    }
+//
+//    $nextRowDirection = null;
+//    $nextColumnDirection = null;
+//
+//    if ($closestCoinPath !== null) {
+//        $nextPointPosition = $closestCoinPath[1];
+//
+//        if ($nextPointPosition[0] > $playerPosition[0]) {
+//            $nextRowDirection = 'S';
+//        } elseif ($nextPointPosition[0] < $playerPosition[0]) {
+//            $nextRowDirection = 'N';
+//        }
+//
+//        if ($nextPointPosition[1] < $playerPosition[1]) {
+//            $nextColumnDirection = 'W';
+//        } elseif ($nextPointPosition[1] > $playerPosition[1]) {
+//            $nextColumnDirection = 'E';
+//        }
+//
+//        if ($nextRowDirection) {
+//            if ($nextRowDirection === $playerDirection) {
+//                $final_move = 'M';
+//            } else {
+//                $directionIndex = $playerDirection . $nextRowDirection;
+//
+//                $direction = $directions[$directionIndex];
+//
+//                $final_move = $direction;
+//            }
+//        }
+//
+//        if ($nextColumnDirection) {
+//            if ($nextColumnDirection === $playerDirection) {
+//                $final_move = 'M';
+//            } else {
+//                $directionIndex = $playerDirection . $nextColumnDirection;
+//
+//                $direction = $directions[$directionIndex];
+//
+//                $final_move = $direction;
+//            }
+//        }
+//    }
+
+
+    return [
+        'move' => $move
+    ];
 });
 
 dispatch();
